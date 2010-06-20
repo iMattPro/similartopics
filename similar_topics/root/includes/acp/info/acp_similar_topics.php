ff554e64767293d133025e5fb27ac1d2bf400052
Matt <maf675@gmail.com>
2010-06-20 10:27:07 -0700

    Mark up all versions to 1.1.1

--- similar_topics/root/includes/acp/info/acp_similar_topics.php
+++ similar_topics/root/includes/acp/info/acp_similar_topics.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package acp
-* @version $Id: acp_similar_topics.php 3 6/15/10 10:37 PM VSE $
+* @version $Id: acp_similar_topics.php 4 6/20/10 10:25 AM VSE $
 * @copyright (c) 2010 Matt Friedman
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
@@ -26,7 +26,7 @@ class acp_similar_topics_info
 		return array(
 			'filename'	=> 'acp_similar_topics',
 			'title'		=> 'PST_TITLE',
-			'version'	=> '1.1.0',
+			'version'	=> '1.1.1',
 			'modes'		=> array(
 				'index'	=> array(
 					'title'			=> 'PST_TITLE',
