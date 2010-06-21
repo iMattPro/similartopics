0ca9d8a95378168bd77335835544bd37b6e2a667
Matt <maf675@gmail.com>
2010-06-21 13:37:42 -0700

    Improve SQL query by removing order by. Added support for non-English stop-word filtering.

--- similar_topics/root/includes/similar_topics.php
+++ similar_topics/root/includes/similar_topics.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package - Precise Similar Topics II
-* @version $Id: similar_topics.php, 11 6/18/10 10:47 PM VSE $
+* @version $Id: similar_topics.php, 12 6/21/10 1:35 PM VSE $
 * @copyright (c) Matt Friedman, Tobias Schäfer, Xabi
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
@@ -42,13 +42,16 @@ function similar_topics(&$topic_data, $forum_id)
 		}
 	}
 
+	// Use phpBB's stop-words if non-English language is detected
+	$topic_title = ($user->lang_name != 'en' && $user->lang_name != 'en_us') ? filter_stop_words($topic_data['topic_title']) : $topic_data['topic_title'];
+
 	// If similar topics is enabled and the number of topics to show is <> 0, proceed...
 	if ($config['similar_topics'] && $config['similar_topics_limit'])
 	{
 		$sql_array = array(
 			'SELECT'	=> 'f.forum_id, f.forum_name, 
 				t.topic_id, t.topic_title, t.topic_time, t.topic_views, t.topic_replies, t.topic_poster, t.topic_first_poster_name, t.topic_first_poster_colour, 
-				MATCH (t.topic_title) AGAINST (\'' . $db->sql_escape($topic_data['topic_title']) . '\') as score',
+				MATCH (t.topic_title) AGAINST (\'' . $db->sql_escape($topic_title) . '\') as score',
 		
 			'FROM'		=> array(
 				TOPICS_TABLE	=> 't',
@@ -61,14 +64,14 @@ function similar_topics(&$topic_data, $forum_id)
 				)
 			),
 
-			'WHERE'		=> "MATCH (t.topic_title) AGAINST ('" . $db->sql_escape($topic_data['topic_title']) . "') >= 0.5
+			'WHERE'		=> "MATCH (t.topic_title) AGAINST ('" . $db->sql_escape($topic_title) . "') >= 0.5
 				AND t.topic_status <> " . ITEM_MOVED . '
 				AND t.topic_time > (UNIX_TIMESTAMP() - ' . $config['similar_topics_time'] . ')
 				AND t.topic_id <> ' . (int) $topic_data['topic_id'],
 
-			'GROUP_BY'	=> 't.topic_id',
+//			'GROUP_BY'	=> 't.topic_id',
 
-			'ORDER_BY'	=> 'score DESC',
+//			'ORDER_BY'	=> 'score DESC',
 		);
 
 		// Now lets see if the current forum is set to search a specific forum search group, and search only those forums
@@ -84,28 +87,75 @@ function similar_topics(&$topic_data, $forum_id)
 
 		$sql = $db->sql_build_query('SELECT', $sql_array);
 		$result = $db->sql_query_limit($sql, $config['similar_topics_limit'], 0, $config['similar_topics_cache']);
-		$similar_topics = $db->sql_fetchrowset($result);
-		$db->sql_freeresult($result);
 
-		if (sizeof($similar_topics))
+		while ($similar = $db->sql_fetchrow($result))
 		{
-			foreach ($similar_topics as $similar)
+			if ($auth->acl_get('f_read', $similar['forum_id']))
 			{
-				if ($auth->acl_get('f_read', $similar['forum_id']))
-				{
-					$template->assign_block_vars('similar', array(
-						'TOPIC_TITLE'		=> $similar['topic_title'],
-						'TOPIC_VIEWS'		=> $similar['topic_views'],
-						'TOPIC_REPLIES'		=> $similar['topic_replies'],
-						'TOPIC_TIME'		=> $user->format_date($similar['topic_time']),
-						'TOPIC_AUTHOR_FULL'	=> get_username_string('full', $similar['topic_poster'], $similar['topic_first_poster_name'], $similar['topic_first_poster_colour']),
-						'U_TOPIC'			=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", "f=" . $similar['forum_id'] . '&amp;t=' . $similar['topic_id']),
-						'U_FORUM'			=> append_sid("{$phpbb_root_path}viewforum.$phpEx", "f=" . $similar['forum_id']),
-						'FORUM'				=> $similar['forum_name'])
-					);
-				}
+				$template->assign_block_vars('similar', array(
+					'TOPIC_TITLE'		=> $similar['topic_title'],
+					'TOPIC_VIEWS'		=> $similar['topic_views'],
+					'TOPIC_REPLIES'		=> $similar['topic_replies'],
+					'TOPIC_TIME'		=> $user->format_date($similar['topic_time']),
+					'TOPIC_AUTHOR_FULL'	=> get_username_string('full', $similar['topic_poster'], $similar['topic_first_poster_name'], $similar['topic_first_poster_colour']),
+					'U_TOPIC'			=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", "f=" . $similar['forum_id'] . '&amp;t=' . $similar['topic_id']),
+					'U_FORUM'			=> append_sid("{$phpbb_root_path}viewforum.$phpEx", "f=" . $similar['forum_id']),
+					'FORUM'				=> $similar['forum_name'])
+				);
+			}
+		}
+		$db->sql_freeresult($result);
+	}
+}
+
+/**
+* MySQL full-text has built-in English stop words. Use phpBB's stop words for non-english languages
+* This will remove uppercases, handle utf8 characters, and ignore words of 2 characters or less
+* Based on a function by phpbb-seo.com
+* 
+* @param  string $text			The topic title
+* @return string $text			The topic titled with any stop-words removed
+*/
+function filter_stop_words($text)
+{
+	$word_list = array();
+	$text = trim(preg_replace('`[\s]+`', ' ', $text));
+	if (!empty($text))
+	{
+		// Put all unique words in the title into an array, and remove uppercases
+		$word_list = array_unique(explode(' ', utf8_strtolower($text)));
+		foreach ($word_list as $key => $word)
+		{
+			// Lets eliminate all words of 2 characters or less
+			if (utf8_strlen(trim($word)) < 3)
+			{
+				unset($word_list[$key]);
 			}
 		}
 	}
+
+	// Remove any stop words from our array
+	if (!empty($word_list))
+	{
+		global $phpbb_root_path, $user, $phpEx;
+		// add stop words to $user session to allow reuse
+		if (empty($user->stop_words))
+		{
+			$words = array();
+			if (file_exists("{$user->lang_path}{$user->lang_name}/search_ignore_words.$phpEx"))
+			{
+				// include the file containing ignore words
+				include("{$user->lang_path}{$user->lang_name}/search_ignore_words.$phpEx");
+			}
+			$user->stop_words = &$words;
+			unset($words);
+		}
+		$word_list = array_diff($word_list, $user->stop_words);
+	}
+
+	// Rebuild our cleaned up topic title
+	$text = !empty($word_list) ? implode(' ', $word_list) : '';
+	return $text;
 }
+
 ?>
