b4137c437ee337c54f6b03d44c55fe2687088d87
Matt Friedman <maf675@gmail.com>
2013-04-25 23:40:54 -0700

    Migration update module_basenames

--- ext/vse/similartopics/migrations/1_3_0.php
+++ ext/vse/similartopics/migrations/1_3_0.php
@@ -22,7 +22,19 @@ class phpbb_ext_vse_similartopics_migrations_1_3_0 extends phpbb_db_migration
 	public function update_data()
 	{
 		return array(
+			array('custom', array(array($this, 'update_module_basename'))),
 			array('config.update', array('similar_topics_version', '1.3.0')),
 		);
+	}
+
+	public function update_module_basename()
+	{
+		$old_module_basename = 'acp_similar_topics';
+		$new_module_basename = 'phpbb_ext_vse_similartopics_acp_similar_topics_module';
+		
+		$sql = 'UPDATE ' . $this->table_prefix . "modules
+			SET module_basename = '$new_module_basename'
+			WHERE module_basename = '$old_module_basename'";
+		$this->db->sql_query($sql);
 	}
 }
