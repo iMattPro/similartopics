bf73099da01053c2ef65b4fe10a7657bcc117c92
Matt Friedman <maf675@gmail.com>
2013-05-26 21:24:17 -0700

    Update core script to use phpbb drivers

--- ext/vse/similartopics/core/similar_topics.php
+++ ext/vse/similartopics/core/similar_topics.php
@@ -17,61 +17,52 @@ if (!defined('IN_PHPBB'))
 
 class phpbb_ext_vse_similartopics_core_similar_topics
 {
-	/**
-	 * Are similar topics enabled?
-	 */
-	private $is_active		= false;
+	/** @var phpbb_auth */
+	protected $auth;
 
-	/**
-	 * The maximum number of similar topics to display
-	 */
-	private $topic_limit	= 5;
+	/** @var phpbb_cache_service */
+	protected $cache;
 
-	/**
-	 * The maximum age of similar topics to display (in days)
-	 */
-	private $topic_age		= 365;
+	/** @var phpbb_config */
+	protected $config;
 
-	/**
-	 * Cache static SQL queries for similar topics
-	 */
-	private $cache_time		= 0;
+	/** @var phpbb_db_driver */
+	protected $db;
 
-	/**
-	 * String of words defined in ACP to be ignored in similar topic searches
-	 */
-	private $ignore_words	= '';
+	/** @var phpbb_template */
+	protected $template;
 
-	/**
-	 * String of forum IDs that are not to be searched for similar topics
-	 */
-	private $ignore_forums	= '';
+	/** @var phpbb_user */
+	protected $user;
 
-	/**
-	 * Is the current forum allowed to display similar topics?
-	 */
-	private $forum_allowed	= true;
+	/** @var string */
+	protected $root_path;
 
+	/** @var string */
+	protected $php_ext;
+	
 	/**
-	 * Is the board using a MySQL database?
+	 * Similar topics constructor method
+	 * 
+	 * @param phpbb_auth $auth
+	 * @param phpbb_cache_service $cache
+	 * @param phpbb_config $config
+	 * @param phpbb_db_driver $db
+	 * @param phpbb_template $template
+	 * @param phpbb_user $user
+	 * @param string $root_path
+	 * @param string $php_ext
 	 */
-	private $mysql_db		= true;
-
-	/**
-	 * Similar topics class constructor method
-	 */
-	public function __construct()
+	public function __construct(phpbb_auth $auth, phpbb_cache_service $cache, phpbb_config $config, phpbb_db_driver $db, phpbb_template $template, phpbb_user $user, $root_path, $php_ext)
 	{
-		global $config, $db, $forum_id;
-
-		$this->is_active     = (bool) $config['similar_topics'];
-		$this->topic_limit   = (int) $config['similar_topics_limit'];
-		$this->topic_age     = (int) $config['similar_topics_time'];
-		$this->cache_time    = (int) $config['similar_topics_cache'];
-		$this->ignore_words  = (string) $config['similar_topics_words'];
-		$this->ignore_forums = (string) $config['similar_topics_ignore'];
-		$this->forum_allowed = (!in_array($forum_id, explode(',', $config['similar_topics_hide']))) ? true : false;
-		$this->mysql_db      = (($db->sql_layer == 'mysql4') || ($db->sql_layer == 'mysqli')) ? true : false;
+		$this->auth = $auth;
+		$this->cache = $cache;
+		$this->config = $config;
+		$this->db = $db;
+		$this->template = $template;
+		$this->user = $user;
+		$this->root_path = $root_path;
+		$this->php_ext = $php_ext;
 	}
 
 	/**
@@ -87,10 +78,8 @@ class phpbb_ext_vse_similartopics_core_similar_topics
 	 */
 	public function get_similar_topics($event)
 	{
-		global $auth, $cache, $config, $user, $db, $template, $phpbb_root_path, $phpEx;
-
-		// All reasons to bail out of the MOD
-		if (!$this->is_active || !$this->mysql_db || !$this->topic_limit || !$this->forum_allowed)
+		// Potential reasons to stop execution
+		if (!$this->config['similar_topics_limit'] || (($this->db->sql_layer != 'mysql4') && ($this->db->sql_layer != 'mysqli')) || (in_array($event['forum_id'], explode(',', $this->config['similar_topics_hide']))))
 		{
 			return;
 		}
@@ -106,7 +95,7 @@ class phpbb_ext_vse_similartopics_core_similar_topics
 		// Similar Topics query
 		$sql_array = array(
 			'SELECT'	=> "f.forum_id, f.forum_name, t.*, 
-				MATCH (t.topic_title) AGAINST ('" . $db->sql_escape($topic_title) . "') AS score",
+				MATCH (t.topic_title) AGAINST ('" . $this->db->sql_escape($topic_title) . "') AS score",
 
 			'FROM'		=> array(
 				TOPICS_TABLE	=> 't',
@@ -119,10 +108,10 @@ class phpbb_ext_vse_similartopics_core_similar_topics
 				),
 			),
 
-			'WHERE'		=> "MATCH (t.topic_title) AGAINST ('" . $db->sql_escape($topic_title) . "') >= 0.5
+			'WHERE'		=> "MATCH (t.topic_title) AGAINST ('" . $this->db->sql_escape($topic_title) . "') >= 0.5
 				AND t.topic_status <> " . ITEM_MOVED . '
 				AND t.topic_approved = 1
-				AND t.topic_time > (UNIX_TIMESTAMP() - ' . $this->topic_age . ')
+				AND t.topic_time > (UNIX_TIMESTAMP() - ' . $this->config['similar_topics_time'] . ')
 				AND t.topic_id <> ' . (int) $event['topic_data']['topic_id'],
 
 		//	'GROUP_BY'	=> 't.topic_id',
@@ -130,120 +119,120 @@ class phpbb_ext_vse_similartopics_core_similar_topics
 		);
 
 		// Add topic tracking data to the query (only when query caching is off)
-		if ($user->data['is_registered'] && $config['load_db_lastread'] && !$this->cache_time)
+		if ($this->user->data['is_registered'] && $this->config['load_db_lastread'] && !$this->config['similar_topics_cache'])
 		{
-			$sql_array['LEFT_JOIN'][] = array('FROM' => array(TOPICS_TRACK_TABLE => 'tt'), 'ON' => 'tt.topic_id = t.topic_id AND tt.user_id = ' . $user->data['user_id']);
-			$sql_array['LEFT_JOIN'][] = array('FROM' => array(FORUMS_TRACK_TABLE => 'ft'), 'ON' => 'ft.forum_id = f.forum_id AND ft.user_id = ' . $user->data['user_id']);
+			$sql_array['LEFT_JOIN'][] = array('FROM' => array(TOPICS_TRACK_TABLE => 'tt'), 'ON' => 'tt.topic_id = t.topic_id AND tt.user_id = ' . $this->user->data['user_id']);
+			$sql_array['LEFT_JOIN'][] = array('FROM' => array(FORUMS_TRACK_TABLE => 'ft'), 'ON' => 'ft.forum_id = f.forum_id AND ft.user_id = ' . $this->user->data['user_id']);
 			$sql_array['SELECT'] .= ', tt.mark_time, ft.mark_time as f_mark_time';
 		}
-		else if ($config['load_anon_lastread'] || $user->data['is_registered'])
+		else if ($this->config['load_anon_lastread'] || $this->user->data['is_registered'])
 		{
 			// Cookie based tracking copied from search.php
-			$tracking_topics = (isset($_COOKIE[$config['cookie_name'] . '_track'])) ? ((STRIP) ? stripslashes($_COOKIE[$config['cookie_name'] . '_track']) : $_COOKIE[$config['cookie_name'] . '_track']) : '';
+			$tracking_topics = (isset($_COOKIE[$this->config['cookie_name'] . '_track'])) ? ((STRIP) ? stripslashes($_COOKIE[$this->config['cookie_name'] . '_track']) : $_COOKIE[$this->config['cookie_name'] . '_track']) : '';
 			$tracking_topics = ($tracking_topics) ? tracking_unserialize($tracking_topics) : array();
 		}
 
 		// Now lets see if the current forum is set to search only in specified forums
 		if (!empty($event['topic_data']['similar_topic_forums']))
 		{
-			$sql_array['WHERE'] .= ' AND ' . $db->sql_in_set('f.forum_id', explode(',', $event['topic_data']['similar_topic_forums']));
+			$sql_array['WHERE'] .= ' AND ' . $this->db->sql_in_set('f.forum_id', explode(',', $event['topic_data']['similar_topic_forums']));
 		}
 		// Otherwise, lets see what forums are not allowed to be searched, and ignore those
-		else if (!empty($this->ignore_forums))
+		else if (!empty($this->config['similar_topics_ignore']))
 		{
-			$sql_array['WHERE'] .= ' AND ' . $db->sql_in_set('f.forum_id', explode(',', $this->ignore_forums), true);
+			$sql_array['WHERE'] .= ' AND ' . $this->db->sql_in_set('f.forum_id', explode(',', $this->config['similar_topics_ignore']), true);
 		}
 
-		$sql = $db->sql_build_query('SELECT', $sql_array);
-		$result = $db->sql_query_limit($sql, $this->topic_limit, 0, $this->cache_time);
+		$sql = $this->db->sql_build_query('SELECT', $sql_array);
+		$result = $this->db->sql_query_limit($sql, $this->config['similar_topics_limit'], 0, $this->config['similar_topics_cache']);
 
 		// Grab icons
-		$icons = $cache->obtain_icons();
+		$icons = $this->cache->obtain_icons();
 
 		$rowset = array();
 
-		while ($row = $db->sql_fetchrow($result))
+		while ($row = $this->db->sql_fetchrow($result))
 		{
 			$similar_forum_id = (int) $row['forum_id'];
 			$similar_topic_id = (int) $row['topic_id'];
 			$rowset[$similar_topic_id] = $row;
 
-			if ($auth->acl_get('f_read', $similar_forum_id))
+			if ($this->auth->acl_get('f_read', $similar_forum_id))
 			{
 				// Get topic tracking info
-				if ($user->data['is_registered'] && $config['load_db_lastread'] && !$this->cache_time)
+				if ($this->user->data['is_registered'] && $this->config['load_db_lastread'] && !$this->config['similar_topics_cache'])
 				{
 					$topic_tracking_info = get_topic_tracking($similar_forum_id, $similar_topic_id, $rowset, array($similar_forum_id => $row['f_mark_time']));
 				}
-				else if ($config['load_anon_lastread'] || $user->data['is_registered'])
+				else if ($this->config['load_anon_lastread'] || $this->user->data['is_registered'])
 				{
 					$topic_tracking_info = get_complete_topic_tracking($similar_forum_id, $similar_topic_id);
 
-					if (!$user->data['is_registered'])
+					if (!$this->user->data['is_registered'])
 					{
-						$user->data['user_lastmark'] = (isset($tracking_topics['l'])) ? (int) (base_convert($tracking_topics['l'], 36, 10) + $config['board_startdate']) : 0;
+						$this->user->data['user_lastmark'] = (isset($tracking_topics['l'])) ? (int) (base_convert($tracking_topics['l'], 36, 10) + $this->config['board_startdate']) : 0;
 					}
 				}
 
 				$folder_img = $folder_alt = $topic_type = '';
-				$replies = ($auth->acl_get('m_approve', $similar_forum_id)) ? $row['topic_replies_real'] : $row['topic_replies'];
+				$replies = ($this->auth->acl_get('m_approve', $similar_forum_id)) ? $row['topic_replies_real'] : $row['topic_replies'];
 				$unread_topic = (isset($topic_tracking_info[$similar_topic_id]) && $row['topic_last_post_time'] > $topic_tracking_info[$similar_topic_id]) ? true : false;
 				topic_status($row, $replies, $unread_topic, $folder_img, $folder_alt, $topic_type);
 
-				$topic_unapproved = (!$row['topic_approved'] && $auth->acl_get('m_approve', $similar_forum_id)) ? true : false;
-				$posts_unapproved = ($row['topic_approved'] && $row['topic_replies'] < $row['topic_replies_real'] && $auth->acl_get('m_approve', $similar_forum_id)) ? true : false;
-				$u_mcp_queue = ($topic_unapproved || $posts_unapproved) ? append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=queue&amp;mode=' . (($topic_unapproved) ? 'approve_details' : 'unapproved_posts') . '&amp;t=' . $similar_topic_id, true, $user->session_id) : '';
+				$topic_unapproved = (!$row['topic_approved'] && $this->auth->acl_get('m_approve', $similar_forum_id)) ? true : false;
+				$posts_unapproved = ($row['topic_approved'] && $row['topic_replies'] < $row['topic_replies_real'] && $this->auth->acl_get('m_approve', $similar_forum_id)) ? true : false;
+				$u_mcp_queue = ($topic_unapproved || $posts_unapproved) ? append_sid("{$this->root_path}mcp.$this->php_ext", 'i=queue&amp;mode=' . (($topic_unapproved) ? 'approve_details' : 'unapproved_posts') . '&amp;t=' . $similar_topic_id, true, $this->user->session_id) : '';
 
-				$base_url = append_sid("{$phpbb_root_path}viewtopic.$phpEx", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id);
+				$base_url = append_sid("{$this->root_path}viewtopic.$this->php_ext", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id);
 
-				$template->assign_block_vars('similar', array(
+				$this->template->assign_block_vars('similar', array(
 					'TOPIC_AUTHOR_FULL'		=> get_username_string('full', $row['topic_poster'], $row['topic_first_poster_name'], $row['topic_first_poster_colour']),
-					'FIRST_POST_TIME'		=> $user->format_date($row['topic_time']),
-					'LAST_POST_TIME'		=> $user->format_date($row['topic_last_post_time']),
+					'FIRST_POST_TIME'		=> $this->user->format_date($row['topic_time']),
+					'LAST_POST_TIME'		=> $this->user->format_date($row['topic_last_post_time']),
 					'LAST_POST_AUTHOR_FULL'	=> get_username_string('full', $row['topic_last_poster_id'], $row['topic_last_poster_name'], $row['topic_last_poster_colour']),
 
-					'PAGE_NUMBER'			=> phpbb_on_page($template, $user, $base_url, $replies + 1, $config['posts_per_page'], 1), 
+					'PAGE_NUMBER'			=> phpbb_on_page($this->template, $this->user, $base_url, $replies + 1, $this->config['posts_per_page'], 1), 
 					'TOPIC_REPLIES'			=> $replies,
 					'TOPIC_VIEWS'			=> $row['topic_views'],
 					'TOPIC_TITLE'			=> $row['topic_title'],
 					'FORUM_TITLE'			=> $row['forum_name'],
 
 					'TOPIC_IMG_STYLE'		=> $folder_img,
-					'TOPIC_FOLDER_IMG'		=> $user->img($folder_img, $folder_alt),
-					'TOPIC_FOLDER_IMG_ALT'	=> $user->lang[$folder_alt],
+					'TOPIC_FOLDER_IMG'		=> $this->user->img($folder_img, $folder_alt),
+					'TOPIC_FOLDER_IMG_ALT'	=> $this->user->lang[$folder_alt],
 
 					'TOPIC_ICON_IMG'		=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['img'] : '',
 					'TOPIC_ICON_IMG_WIDTH'	=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['width'] : '',
 					'TOPIC_ICON_IMG_HEIGHT'	=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['height'] : '',
-					'ATTACH_ICON_IMG'		=> ($auth->acl_get('u_download') && $auth->acl_get('f_download', $similar_forum_id) && $row['topic_attachment']) ? $user->img('icon_topic_attach', $user->lang['TOTAL_ATTACHMENTS']) : '',
-					'UNAPPROVED_IMG'		=> ($topic_unapproved || $posts_unapproved) ? $user->img('icon_topic_unapproved', ($topic_unapproved) ? 'TOPIC_UNAPPROVED' : 'POSTS_UNAPPROVED') : '',
+					'ATTACH_ICON_IMG'		=> ($this->auth->acl_get('u_download') && $this->auth->acl_get('f_download', $similar_forum_id) && $row['topic_attachment']) ? $this->user->img('icon_topic_attach', $this->user->lang['TOTAL_ATTACHMENTS']) : '',
+					'UNAPPROVED_IMG'		=> ($topic_unapproved || $posts_unapproved) ? $this->user->img('icon_topic_unapproved', ($topic_unapproved) ? 'TOPIC_UNAPPROVED' : 'POSTS_UNAPPROVED') : '',
 
 					'S_UNREAD_TOPIC'		=> $unread_topic,
-					'S_TOPIC_REPORTED'		=> (!empty($row['topic_reported']) && $auth->acl_get('m_report', $similar_forum_id)) ? true : false,
+					'S_TOPIC_REPORTED'		=> (!empty($row['topic_reported']) && $this->auth->acl_get('m_report', $similar_forum_id)) ? true : false,
 					'S_TOPIC_UNAPPROVED'	=> $topic_unapproved,
 					'S_POSTS_UNAPPROVED'	=> $posts_unapproved,
 
-					'U_NEWEST_POST'			=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id . '&amp;view=unread') . '#unread',
-					'U_LAST_POST'			=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id . '&amp;p=' . $row['topic_last_post_id']) . '#p' . $row['topic_last_post_id'],
-					'U_VIEW_TOPIC'			=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id),
-					'U_VIEW_FORUM'			=> append_sid("{$phpbb_root_path}viewforum.$phpEx", 'f=' . $similar_forum_id),
-					'U_MCP_REPORT'			=> append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=reports&amp;mode=reports&amp;f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id, true, $user->session_id),
+					'U_NEWEST_POST'			=> append_sid("{$this->root_path}viewtopic.$this->php_ext", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id . '&amp;view=unread') . '#unread',
+					'U_LAST_POST'			=> append_sid("{$this->root_path}viewtopic.$this->php_ext", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id . '&amp;p=' . $row['topic_last_post_id']) . '#p' . $row['topic_last_post_id'],
+					'U_VIEW_TOPIC'			=> append_sid("{$this->root_path}viewtopic.$this->php_ext", 'f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id),
+					'U_VIEW_FORUM'			=> append_sid("{$this->root_path}viewforum.$this->php_ext", 'f=' . $similar_forum_id),
+					'U_MCP_REPORT'			=> append_sid("{$this->root_path}mcp.$this->php_ext", 'i=reports&amp;mode=reports&amp;f=' . $similar_forum_id . '&amp;t=' . $similar_topic_id, true, $this->user->session_id),
 					'U_MCP_QUEUE'			=> $u_mcp_queue,
 				));
 
-				phpbb_generate_template_pagination($template, $base_url, 'similar.pagination', 'start', $replies + 1, $config['posts_per_page'], 1, true, true);
+				phpbb_generate_template_pagination($this->template, $base_url, 'similar.pagination', 'start', $replies + 1, $this->config['posts_per_page'], 1, true, true);
 			}
 		}
 
-		$db->sql_freeresult($result);
+		$this->db->sql_freeresult($result);
 
-		$user->add_lang_ext('vse/similartopics', 'similar_topics');
+		$this->user->add_lang_ext('vse/similartopics', 'similar_topics');
 
-		$template->assign_vars(array(
-			'L_SIMILAR_TOPICS'	=> $user->lang['SIMILAR_TOPICS'],
-			'NEWEST_POST_IMG'	=> $user->img('icon_topic_newest', 'VIEW_NEWEST_POST'),
-			'LAST_POST_IMG'		=> $user->img('icon_topic_latest', 'VIEW_LATEST_POST'),
-			'REPORTED_IMG'		=> $user->img('icon_topic_reported', 'TOPIC_REPORTED'),
+		$this->template->assign_vars(array(
+			'L_SIMILAR_TOPICS'	=> $this->user->lang['SIMILAR_TOPICS'],
+			'NEWEST_POST_IMG'	=> $this->user->img('icon_topic_newest', 'VIEW_NEWEST_POST'),
+			'LAST_POST_IMG'		=> $this->user->img('icon_topic_latest', 'VIEW_LATEST_POST'),
+			'REPORTED_IMG'		=> $this->user->img('icon_topic_reported', 'TOPIC_REPORTED'),
 		));
 	}
 
@@ -256,13 +245,11 @@ class phpbb_ext_vse_similartopics_core_similar_topics
 	 */
 	private function strip_topic_title($text)
 	{
-		global $user;
-
 		// Strip quotes, ampersands
 		$text = str_replace(array('&quot;', '&amp;'), '', $text);
 
-		$english_lang = ($user->lang_name == 'en' || $user->lang_name == 'en_us') ? true : false;
-		$ignore_words = !empty($this->ignore_words) ? true : false;
+		$english_lang = ($this->user->lang_name == 'en' || $this->user->lang_name == 'en_us') ? true : false;
+		$ignore_words = !empty($this->config['similar_topics_words']) ? true : false;
 
 		if (!$english_lang || $ignore_words)
 		{
@@ -283,20 +270,18 @@ class phpbb_ext_vse_similartopics_core_similar_topics
 	 */
 	private function strip_stop_words($text, $english_lang, $ignore_words)
 	{
-		global $user, $phpEx;
-
 		$words = array();
 
-		if (!$english_lang && file_exists("{$user->lang_path}{$user->lang_name}/search_ignore_words.$phpEx"))
+		if (!$english_lang && file_exists("{$this->user->lang_path}{$this->user->lang_name}/search_ignore_words.$this->php_ext"))
 		{
 			// Retrieve a language dependent list of words to be ignored (method copied from search.php)
-			include("{$user->lang_path}{$user->lang_name}/search_ignore_words.$phpEx");
+			include("{$this->user->lang_path}{$this->user->lang_name}/search_ignore_words.$this->php_ext");
 		}
 
 		if ($ignore_words)
 		{
 			// Merge any custom defined ignore words from the ACP to the stop-words array
-			$words = array_merge($this->make_word_array($this->ignore_words), $words);
+			$words = array_merge($this->make_word_array($this->config['similar_topics_words']), $words);
 		}
 
 		// Remove stop-words from the topic title text
