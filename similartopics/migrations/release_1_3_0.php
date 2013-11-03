302cd5028d966727e312dcb572d3ffd81bb76ad0
Matt Friedman <maf675@gmail.com>
2013-11-03 11:55:03 -0800

    Update module mode during migration

--- similartopics/migrations/release_1_3_0.php
+++ similartopics/migrations/release_1_3_0.php
@@ -46,19 +46,29 @@ class release_1_3_0 extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		return array(
-			array('custom', array(array($this, 'update_module_basename'))),
+			array('custom', array(array($this, 'update_module'))),
 			array('config.update', array('similar_topics_version', '1.3.0')),
 		);
 	}
 
-	public function update_module_basename()
+	public function update_module()
 	{
+		// Update the module basename if neccessary
 		$old_module_basename = 'acp_similar_topics';
 		$new_module_basename = '\vse\similartopics\acp\similar_topics_module';
 		
 		$sql = 'UPDATE ' . $this->table_prefix . "modules
 			SET module_basename = '" . $this->db->sql_escape($new_module_basename) . "'
 			WHERE module_basename = '$old_module_basename'";
+		$this->db->sql_query($sql);
+
+		// Update the module mode if neccessary
+		$old_module_mode = 'index';
+		$new_module_mode = 'settings';
+		
+		$sql = 'UPDATE ' . $this->table_prefix . "modules
+			SET module_mode = '" . $this->db->sql_escape($new_module_mode) . "'
+			WHERE module_basename = '" . $this->db->sql_escape($new_module_basename) . "' AND module_mode = '$old_module_mode'";
 		$this->db->sql_query($sql);
 	}
 }
