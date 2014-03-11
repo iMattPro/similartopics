c2973b55913c51cce9fb63d44931cc502c2c2989
Matt Friedman <maf675@gmail.com>
2014-03-11 16:47:24 -0700

    Add the rest of the topics to test

--- tests/functional/similar_topics_test.php
+++ tests/functional/similar_topics_test.php
@@ -91,16 +91,16 @@ class extension_functional_similar_topics_test extends extension_functional_test
 	public function test_similar_topics()
 	{
 		// Create some basic topics
-// 		$post1 = $this->create_topic(2, 'Test Topic 1', 'This is test topic 1 posted by the testing framework.');
-// 		$post2 = $this->create_topic(2, 'Test Topic 2', 'This is test topic 2 posted by the testing framework.');
-// 		$post3 = $this->create_topic(2, 'Test Topic 3', 'This is test topic 3 posted by the testing framework.');
-// 		$post4 = $this->create_topic(2, 'Test Framework Topic 4', 'This is test topic 4 posted by the testing framework.');
+		$post1 = $this->create_topic(2, 'Test Topic 1', 'This is test topic 1 posted by the testing framework.');
+		$post2 = $this->create_topic(2, 'Test Topic 2', 'This is test topic 2 posted by the testing framework.');
+		$post3 = $this->create_topic(2, 'Test Topic 3', 'This is test topic 3 posted by the testing framework.');
+		$post4 = $this->create_topic(2, 'Test Framework Topic 4', 'This is test topic 4 posted by the testing framework.');
 		$post5 = $this->create_topic(2, 'Test Framework Topic 5', 'This is test topic 5 posted by the testing framework.');
 
 		// Load topic #5
 		$crawler = self::request('GET', "viewtopic.php?t={$post5['topic_id']}&sid={$this->sid}");
 		
 		// Test that the title of topic #4 is found
-//		$this->assertContains('Test Framework Topic 4', $crawler->filter('html')->text());
+		$this->assertContains('Test Framework Topic 4', $crawler->filter('html')->text());
 	}
 }
