0460d08e96129faa6613c431778c133b6404df31
Matt Friedman <maf675@gmail.com>
2014-03-14 15:44:37 -0700

    Remove hard coded language from functional tests

--- tests/functional/similar_topics_ucp_test.php
+++ tests/functional/similar_topics_ucp_test.php
@@ -16,10 +16,9 @@ class similar_topics_ucp_test extends similar_topics_base
 {
 	public function test_ucp_pages()
 	{
-		//$this->add_lang_ext('similiar_topics');
+		$this->add_lang_ext('similiar_topics');
 
 		$crawler = self::request('GET', 'ucp.php?i=ucp_prefs&mode=view&sid=' . $this->sid);
-		$this->assertContains('Display similar topics', $crawler->filter('#cp-main')->text());
-		//$this->assertContains($this->lang('DISPLAY_SIMILAR_TOPICS'), $crawler->filter('#cp-main')->text());
+		$this->assertContains($this->lang('DISPLAY_SIMILAR_TOPICS'), $crawler->filter('#cp-main')->text());
 	}
 }
