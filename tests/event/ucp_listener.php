c5b5815086cfc18e9d52399ddcbebeab4ff7ca7d
Matt Friedman <maf675@gmail.com>
2014-09-19 19:15:51 -0700

    Fix listener tests with UCP split out

---
+++ tests/event/ucp_listener.php
@@ -0,0 +1,110 @@
+<?php
+/**
+*
+* Precise Similar Topics
+*
+* @copyright (c) 2014 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace vse\similartopics\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class ucp_listener implements EventSubscriberInterface
+{
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\auth\auth $auth
+	* @param \phpbb\config\config $config
+	* @param \phpbb\request\request $request
+	* @param \phpbb\template\template $template
+	* @param \phpbb\user $user
+	* @return \vse\similartopics\event\ucp_listener
+	* @access public
+	*/
+	public function __construct(\phpbb\auth\auth $auth, \phpbb\config\config $config, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user)
+	{
+		$this->auth = $auth;
+		$this->config = $config;
+		$this->request = $request;
+		$this->template = $template;
+		$this->user = $user;
+	}
+
+	/**
+	* Assign functions defined in this class to event listeners in the core
+	*
+	* @return array
+	* @static
+	* @access public
+	*/
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.ucp_prefs_view_data'				=> 'ucp_prefs_get_data',
+			'core.ucp_prefs_view_update_data'		=> 'ucp_prefs_set_data',
+		);
+	}
+
+	/**
+	* Get user's Similar Topics option and display it in UCP Prefs View page
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function ucp_prefs_get_data($event)
+	{
+		// Request the user option vars and add them to the data array
+		$event['data'] = array_merge($event['data'], array(
+			'similar_topics'	=> $this->request->variable('similar_topics', (int) $this->user->data['user_similar_topics']),
+		));
+
+		// Output the data vars to the template (except on form submit)
+		if (!$event['submit'])
+		{
+			$data = $event['data'];
+			$this->user->add_lang_ext('vse/similartopics', 'similar_topics');
+			$this->template->assign_vars(array(
+				'S_SIMILAR_TOPICS'			=> $this->config['similar_topics'] && $this->auth->acl_get('u_similar_topics'),
+				'S_DISPLAY_SIMILAR_TOPICS'	=> $data['similar_topics'],
+			));
+		}
+	}
+
+	/**
+	* Add user's Similar Topics option state into the sql_array
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function ucp_prefs_set_data($event)
+	{
+		$data = $event['data'];
+		$event['sql_ary'] = array_merge($event['sql_ary'], array(
+			'user_similar_topics' => $data['similar_topics'],
+		));
+	}
+}
