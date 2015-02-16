b35ef3d27b3511bc64ca6c13bbcd71334fabca1f
Matt Friedman <maf675@gmail.com>
2015-02-16 15:25:00 -0800

    Fix method visibility

--- acp/similar_topics_info.php
+++ acp/similar_topics_info.php
@@ -15,7 +15,7 @@ namespace vse\similartopics\acp;
 */
 class similar_topics_info
 {
-	function module()
+	public function module()
 	{
 		return array(
 			'filename'	=> '\vse\similartopics\acp\similar_topics_module',
