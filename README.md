b70db98329b9800c017f607b3cda65c3ca594361
Matt Friedman <maf675@gmail.com>
2015-05-10 17:14:02 -0700

    Add Greek translation [ci skip]

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ This is an extension for phpBB 3.1 that will find and display a list of similar 
 * Dutch
 * French
 * German
+* Greek
 * Japanese
 * Persian
 * Polish
---
+++ language/el/info_acp_similar_topics.php
@@ -0,0 +1,45 @@
+<?php
+/**
+*
+* Precise Similar Topics [Greek - el]
+*
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'PST_TITLE_ACP'		=> 'Παραπλήσια Θέματα',
+	'PST_SETTINGS'		=> 'Ρυθμίσεις',
+	'PST_LOG_FULLTEXT'	=> '<strong>Συμβατότητα τροποποίησης βάσης δεδομένων για τα "Παραπλήσια Θέματα"</strong><br />» Αλλάχτηκε ο πίνακας "%s" σε μηχανή αποθήκευσης MyISAM και προστέθηκε ένα ευρετήριο FULLTEXT στο topic_title',
+	'PST_LOG_MSG'		=> '<strong>Ρυθμίσεις τροποποίησης "Παραπλήσιων Θεμάτων"</strong>',
+));
