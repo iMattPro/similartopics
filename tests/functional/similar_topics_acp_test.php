29d6659beb8be3b364f6e6f1ea4d36d35f356ad9
Matt Friedman <maf675@gmail.com>
2015-09-16 15:07:43 -0700

    Functional tests for logs and permissions

--- tests/functional/similar_topics_acp_test.php
+++ tests/functional/similar_topics_acp_test.php
@@ -15,6 +15,14 @@ namespace vse\similartopics\tests\functional;
 */
 class similar_topics_acp_test extends similar_topics_base
 {
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->login();
+		$this->admin_login();
+	}
+
 	public function acp_pages_data()
 	{
 		return array(
@@ -28,13 +36,31 @@ class similar_topics_acp_test extends similar_topics_base
 	*/
 	public function test_acp_pages($mode)
 	{
-		$this->login();
-		$this->admin_login();
-
 		$this->add_lang_ext('vse/similartopics', 'acp_similar_topics');
-
 		$crawler = self::request('GET', 'adm/index.php?i=\vse\similartopics\acp\similar_topics_module&amp;mode=' . $mode . '&sid=' . $this->sid);
 		$this->assertContainsLang('PST_TITLE_ACP', $crawler->text());
 		$this->assertContainsLang('PST_EXPLAIN', $crawler->text());
+		return $crawler;
 	}
+
+	public function test_acp_logs()
+	{
+		$this->add_lang_ext('vse/similartopics', array('acp_similar_topics', 'info_acp_similar_topics'));
+		$crawler = self::request('GET', 'adm/index.php?i=\vse\similartopics\acp\similar_topics_module&amp;mode=settings&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$crawler = self::submit($form);
+		$this->assertContainsLang('PST_SAVED', $crawler->text());
+		$crawler = self::request('GET', 'adm/index.php?i=acp_logs&mode=admin&sid=' . $this->sid);
+		$this->assertContains(strip_tags($this->lang('PST_LOG_MSG')), $crawler->text());
+	}
+
+	public function test_acp_permissions()
+	{
+		$this->add_lang_ext('vse/similartopics', 'permissions_similar_topics');
+		$crawler = self::request('GET', 'adm/index.php?i=acp_permissions&mode=setting_group_global&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$crawler = self::submit($form);
+		$this->assertContainsLang('ACL_U_SIMILARTOPICS', $crawler->text());
+	}
+
 }
