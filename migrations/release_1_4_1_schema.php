2b6c0dc28b0e48a0522fac7d05d21373b74a3a28
Matt Friedman <maf675@gmail.com>
2017-01-03 16:55:56 -0800

    Allow null default for updated similar_topic_forums field

---
+++ migrations/release_1_4_1_schema.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * Precise Similar Topics
+ *
+ * @copyright (c) 2017 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace vse\similartopics\migrations;
+
+class release_1_4_1_schema extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\vse\similartopics\migrations\release_1_4_0_schema');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'change_columns'	=> array(
+				$this->table_prefix . 'forums'	=> array(
+					'similar_topic_forums'		=> array('MTEXT', null),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'change_columns'	=> array(
+				$this->table_prefix . 'forums'	=> array(
+					'similar_topic_forums'		=> array('MTEXT', ''),
+				),
+			),
+		);
+	}
+}
---
+++ tests/functional/create_forum_test.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * Precise Similar Topics
+ *
+ * @copyright (c) 2017 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace vse\similartopics\tests\functional;
+
+/**
+ * @group functional
+ */
+class create_forum_test extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('vse/similartopics');
+	}
+
+	public function test_create_forum()
+	{
+		$this->add_lang('acp/forums');
+
+		$this->login();
+		$this->admin_login();
+
+		$crawler = self::request('GET', "adm/index.php?i=acp_forums&mode=manage&sid={$this->sid}");
+		$form = $crawler->selectButton($this->lang('CREATE_FORUM'))->form();
+		$crawler = self::submit($form);
+		$this->assertContainsLang('CREATE_FORUM', $crawler->filter('#main h1')->text());
+
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form(array(
+			'forum_name'	=> 'Test Forum',
+		));
+		$crawler = self::submit($form);
+
+		$this->assertGreaterThan(0, $crawler->filter('.successbox')->count());
+	}
+}
