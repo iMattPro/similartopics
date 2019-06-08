852c2261a5d89ec2c1df458f43b3cb5817a6fae1
Matt Friedman <maf675@gmail.com>
2019-06-08 10:59:07 -0700

    Migrate existing ignore words from config to config_text

---
+++ migrations/release_1_5_x/similar_topic_words.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * Precise Similar Topics
+ *
+ * @copyright (c) 2019 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace vse\similartopics\migrations\release_1_5_x;
+
+class similar_topic_words extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return !$this->config->offsetExists('similar_topics_words');
+	}
+
+	static public function depends_on()
+	{
+		return array('\vse\similartopics\migrations\release_1_1_6_data');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('if', array(
+				!empty($this->config['similar_topics_words']),
+				array('config_text.add', array('similar_topics_words', $this->config['similar_topics_words'])),
+			)),
+			array('config.remove', array('similar_topics_words')),
+		);
+	}
+}
