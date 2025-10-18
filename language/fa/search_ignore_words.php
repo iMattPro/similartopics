c60c2f790422c1f2b75b8100686df7c84cfd8d1f
Matt Friedman <maf675@gmail.com>
2025-10-18 10:15:52 -0700

    Add missing localized stop words translated by AI

---
+++ language/fa/search_ignore_words.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *
+ * Precise Similar Topics
+ *
+ * @copyright (c) 2025 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * This [fa] language list of stop words created by Matt Friedman for Similar Topics
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+   exit;
+}
+
+$words = array(
+	'آن',
+	'آنها',
+	'اگر',
+	'از',
+	'است',
+	'این',
+	'اینها',
+	'با',
+	'باشد',
+	'بر',
+	'برای',
+	'بعد',
+	'بود',
+	'بوده',
+	'بزرگ',
+	'تا',
+	'تمام',
+	'جدید',
+	'خوب',
+	'در',
+	'روز',
+	'روزها',
+	'زمان',
+	'شد',
+	'شده',
+	'شود',
+	'کار',
+	'که',
+	'کرد',
+	'کرده',
+	'کند',
+	'کوچک',
+	'گفت',
+	'ما',
+	'من',
+	'نیز',
+	'هم',
+	'همه',
+	'هیچ',
+	'و',
+	'وقت',
+	'ولی',
+	'یا',
+	'یک',
+);
