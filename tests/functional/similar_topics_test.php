d3cc3259b18386b94d627b8382a78efb2892324b
Matt Friedman <maf675@gmail.com>
2025-10-19 09:15:04 -0700

    Update tests

--- tests/functional/similar_topics_test.php
+++ tests/functional/similar_topics_test.php
@@ -20,20 +20,25 @@ class similar_topics_test extends similar_topics_base
 		$this->login();
 
 		// Create some basic topics
-		$topic1 = $this->create_topic(2, 'Test Topic 1', 'This is test topic 1 posted by the testing framework.');
-		$topic2 = $this->create_topic(2, 'Test Topic 2', 'This is test topic 2 posted by the testing framework.');
-		$topic3 = $this->create_topic(2, 'Test Topic 3', 'This is test topic 3 posted by the testing framework.');
-		$topic4 = $this->create_topic(2, 'Test Framework Topic 4', 'This is test topic 4 posted by the testing framework.');
-		$topic5 = $this->create_topic(2, 'Test Framework Topic 5', 'This is test topic 5 posted by the testing framework.');
+		$topic1 = $this->create_topic(2, 'This is Test Topic 1', 'This is test topic 1 posted by the testing framework.');
+		$topic2 = $this->create_topic(2, 'This is Test Topic 2', 'This is test topic 2 posted by the testing framework.');
+		$topic3 = $this->create_topic(2, 'This is Test Topic 3', 'This is test topic 3 posted by the testing framework.');
+		$topic4 = $this->create_topic(2, 'This is Test Framework Topic 4', 'This is test topic 4 posted by the testing framework.');
+		$topic5 = $this->create_topic(2, 'This is Test Framework Topic 5', 'This is test topic 5 posted by the testing framework.');
+		$topic6 = $this->create_topic(2, 'This is it', 'This is test topic 6 posted by the testing framework.');
 
 		// Load topic #5
 		$crawler = self::request('GET', "viewtopic.php?t={$topic5['topic_id']}&sid=$this->sid");
 		// Test that the title of topic #4 is found
-		self::assertStringContainsString('Test Framework Topic 4', $crawler->filter('html')->text());
+		self::assertStringContainsString('This is Test Framework Topic 4', $crawler->filter('html')->text());
+		// Test that the title of topic #6 is not included (just ignore words)
+		self::assertStringNotContainsString('This is it', $crawler->filter('html')->text());
 
 		// Load topic #4
 		$crawler = self::request('GET', "viewtopic.php?t={$topic4['topic_id']}&sid=$this->sid");
 		// Test that the title of topic #5 is found
-		self::assertStringContainsString('Test Framework Topic 5', $crawler->filter('html')->text());
+		self::assertStringContainsString('This is Test Framework Topic 5', $crawler->filter('html')->text());
+		// Test that the title of topic #6 is not included (just ignore words)
+		self::assertStringNotContainsString('This is it', $crawler->filter('html')->text());
 	}
 }
