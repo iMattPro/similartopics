19f7695020fb10727879e87bca5f1eaef9d58079
Matt Friedman <maf675@gmail.com>
2025-10-22 07:56:08 -0700

    Clean up ext enable checks

--- ext.php
+++ ext.php
@@ -13,29 +13,14 @@ namespace vse\similartopics;
 class ext extends \phpbb\extension\base
 {
 	/**
-	 * Extension requires phpBB 3.2.1 or newer.
+	 * Extension requires phpBB 3.2.1 or newer. Not compatible with phpBB 4.x.
 	 *
-	 * @return array|bool If phpBB 3.3.x, return message as to why it could not be installed.
-	 *                    Otherwise, just return boolean true/false.
+	 * @return bool
 	 */
 	public function is_enableable()
 	{
-		$db = $this->container->get('dbal.conn');
-
-		$enableable =
+		return
 			phpbb_version_compare(PHPBB_VERSION, '3.2.1', '>=') &&
 			phpbb_version_compare(PHPBB_VERSION, '4.0.0-dev', '<');
-
-		// Since showing error messages only works for phpBB 3.3.x, the only message worth showing is
-		// if the DBMS is incompatible. That is, we can't show somebody on phpBB 3.2.x. the message that
-		// their board is too old, so there's no reason to handle invalid phpBB version messages here.
-		if (!$enableable && phpbb_version_compare(PHPBB_VERSION, '3.3.0-b1', '>='))
-		{
-			$lang = $this->container->get('language');
-			$lang->add_lang('acp_similar_topics', 'vse/similartopics');
-			return array($lang->lang('PST_NO_COMPAT'));
-		}
-
-		return $enableable;
 	}
 }
